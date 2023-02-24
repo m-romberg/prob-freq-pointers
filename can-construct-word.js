447d233a433afa2e924fdbb1f231a2b8b3726edc
Madelyn Romberg <madelynromberg@g.ucla.edu>
2023-02-24 15:38:17 -0800

    solve can-sonctruct-word

--- can-construct-word.js
+++ can-construct-word.js
@@ -1,3 +1,37 @@
+'use strict';
 // add whatever parameters you deem necessary & write doc comment
-function canConstructWord() {
+
+/**f
+ * unction takes in word string and letter strings
+ * returns true if letters string can build word, else false
+ * case and frequency sensitive
+ *  */
+
+function canConstructWord(word, letters) {
+
+  const wordFreq = getFrequencyCounter(word);
+  const letterFreq = getFrequencyCounter(letters);
+
+  for (const char in wordFreq){
+    console.log('char', char, "word", wordFreq[char], "letter", letterFreq[char] );
+
+    if (wordFreq[char] > letterFreq[char] || letterFreq[char]=== undefined) return false;
+  }
+  return true;
+
+}
+
+/**
+ * Intakes an item (arr, str, etc)
+ * returns object with elements as key and frequency value
+ */
+function getFrequencyCounter(items) {
+  const freqs = {};
+
+  for (const item of items) {
+    const curr = freqs[item] || 0;
+    freqs[item] = curr + 1;
+  }
+
+  return freqs;
 }
