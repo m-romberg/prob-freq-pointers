8d70e8fdc89937cd7840c36a49aa1671fc7a5882
Madelyn Romberg <madelynromberg@g.ucla.edu>
2023-02-24 15:50:04 -0800

    solve average-pair

--- average-pair.js
+++ average-pair.js
@@ -1,4 +1,26 @@
+'use strict';
+
 // add whatever parameters you deem necessary & write docstring
-function averagePair() {
+
+/**
+ * Function intakes SORTED array of numbers and average number (targetAvg) (UI)
+ * Returns true if a pair averaged equals targetAvg, false if otherwise
+ */
+
+function averagePair(nums, targetAvg) {
+  let left = 0;
+  let right = nums.length -1;
+
+  while (left < right){
+    const avg = (nums[left] + nums[right]) / 2;
+    if (avg === targetAvg) {
+      return true;
+    } else if (avg < targetAvg) {
+      left ++;
+    } else {
+      right --;
+    }
+  }
+  return false;
 }
 
