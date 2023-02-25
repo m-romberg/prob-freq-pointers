095beafc9a55f266f68782c3dcd52ec91e2c6be3
Terry Li <terryseanli@gmail.com>
2023-02-24 16:09:36 -0800

    solved two-array-object and passed tests

--- two-array-object.js
+++ two-array-object.js
@@ -1,3 +1,15 @@
+"use strict";
+
+/** Takes 2 arrays (keys and values)
+ * returns object with keys from keys and values from values: O(n)
+ */
+
 // add whatever parameters you deem necessary & write docstring
-function twoArrayObject() {
+function twoArrayObject(keys, values) {
+  let keysToValues = {};
+  for(let i = 0; i < keys.length; i++) {
+    if (values[i] === undefined) keysToValues[keys[i]] = null;
+    else keysToValues[keys[i]] = values[i];
+  }
+  return keysToValues;
 }
